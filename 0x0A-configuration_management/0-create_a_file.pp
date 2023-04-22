c2557d15a60f14820419708360a1da0b8783ec90
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-22 12:54:20 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create file in /tmp
 
-file { '/tmp/school' :
-    path    => '/tmp/school',
+file { '/tmp/school':
+    ensure => 'file',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
