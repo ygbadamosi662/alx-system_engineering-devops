f77b8ba5142da77e11d975b4c80fcd567ac0f81b
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-05-23 05:57:05 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     employee_url = f'{base_url}/users/{sys.argv[1]}'
     response = requests.get(employee_url)
     if response.status_code != 200:
-        print(f"Failed to retrieve employee details for ID: {sys.argv[1])}")
+        print(f"Failed to retrieve employee details for ID: {sys.argv[1]}")
         return
 
     employee_data = response.json()
     employee_name = employee_data['name']
 
-    todos_url = f'{base_url}/todos?userId={sys.argv[1])}'
+    todos_url = f'{base_url}/todos?userId={sys.argv[1]}'
     response = requests.get(todos_url)
     if response.status_code != 200:
         print(f"Failed to retrieve TODO list for employee: {employee_name}")
