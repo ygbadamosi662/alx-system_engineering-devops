118bd3e8c2f46eb7caa0f4e4a75b562377fb0077
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-05-23 05:52:35 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     base_url = 'https://jsonplaceholder.typicode.com'
-    
+
     employee_url = f'{base_url}/users/{sys.argv[1])}'
     response = requests.get(employee_url)
     if response.status_code != 200:
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     employee_data = response.json()
     employee_name = employee_data['name']
-    
+
     todos_url = f'{base_url}/todos?userId={sys.argv[1])}'
     response = requests.get(todos_url)
     if response.status_code != 200:
