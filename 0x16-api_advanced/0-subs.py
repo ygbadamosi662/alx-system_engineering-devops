60bd1a7f036a0f7ef0f50bc536d6da2acdb76ba0
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-06-07 07:35:14 +0100

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,23 +5,25 @@ Get the total number of subscribers
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    A Python function that queries reddit API to get the total number of subscribers not active userss
+    A Python function that queries reddit API
+    to get the total number of subscribers not active userss
     """
-    
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'+
+    ' (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
     headers = {'User-Agent': userAgent}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            print("HERE" + data['data']['subscribers'])
+
             return data['data']['subscribers']
         else:
             return 0
     except requests.exceptions.RequestException as e:
         return 0
-number_of_subscribers('this_is_a_fake_subreddit')
