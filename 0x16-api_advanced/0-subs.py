20bd3cca20c3cae5ae12f2ee96594f48db88d1b1
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-06-07 07:08:04 +0100

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Get the total number of subscribers
+"""
+
+from requests import get
+
+def number_of_subscribers(subreddit):
+    """
+    A Python function that queries reddit API to get the total number of subscribers not active userss
+    """
+    
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    headers = {'User-Agent': userAgent}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            print(data['data']['subscribers'])
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.exceptions.RequestException as e:
+        return 0
