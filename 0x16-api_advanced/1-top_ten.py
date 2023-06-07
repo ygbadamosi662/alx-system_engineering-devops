83dfef228235c8590559cac806902baa8b7ddd88
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-06-07 08:11:11 +0100

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists top_ten function"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the 10 hottest posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "+
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    headers = {
+        "User-Agent": userAgent
+    }
+    param = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=param,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
